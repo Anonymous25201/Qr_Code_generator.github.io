ede73ba8338402548a02c0c454f09b7b1e121728
Anonymous25201 <91649899+Anonymous25201@users.noreply.github.com>
2023-09-18 17:40:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const generateButton = document.getElementById('generate-button');
 const textInput = document.getElementById('text-input');
 const qrCodeContainer = document.getElementById('qr-code-container');
 const downloadLink = document.getElementById('download-link');
+const whatsappShareLink = document.getElementById('whatsapp-share-link'); // Add this line
 
 generateButton.addEventListener('click', async () => {
     const text = textInput.value;
     if (text.trim() !== '') {
-        const apiEndpoint = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(
+        const apiEndpoint = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example${encodeURIComponent(
             text
-        )}`;
+        )}&size=128x128`;
 
         try {
             const response = await fetch(apiEndpoint);
@@ -20,18 +21,19 @@ generateButton.addEventListener('click', async () => {
                 qrCodeImage.src = URL.createObjectURL(data);
                 qrCodeContainer.innerHTML = '';
                 qrCodeContainer.appendChild(qrCodeImage);
-                const whatsappShareLink = document.getElementById('whatsapp-share-link');
+
                 // Show download link
                 downloadLink.style.display = 'inline-block';
                 downloadLink.href = qrCodeImage.src;
                 downloadLink.download = 'qrcode.png';
 
-                    // Show WhatsApp share link
-                    whatsappShareLink.style.display = 'inline-block';
-                    whatsappShareLink.href = `whatsapp://send?text=Check out this QR Code&image=${encodeURIComponent(
-                        textInput
-                    )}`;
-                
+                // Show WhatsApp share link with image
+                whatsappShareLink.style.display = 'inline-block';
+                const imageUrl = qrCodeImage.src;
+                const shareMessage = 'Check out this QR Code:';
+                whatsappShareLink.href = `whatsapp://send?text=${encodeURIComponent(
+                    shareMessage
+                )}&image=${encodeURIComponent(imageUrl)}`;
             } else {
                 throw new Error('Failed to generate QR code');
             }
